dbe4c2fde22e5c3215c8ec5eccbbf0cc4f40b3a4
KayZ <270159822@yoobeestudent.ac.nz>
2022-08-16 15:54:06 +1200

    two components
    Articles
    Greet

---
+++ components/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Greet name="peter" heroName = "SpiderMan"/>
+    <Greet name="Bruce" heroName = "Batman"/>
+    <Greet :name=" name " :heroName="heroName"/>
+  </div>
+
+    <Article>
+   :title= "someTitle"
+    :likes="50"
+    :isPublished="true"
+    </Article>
+  
+
+</template>
+
+<script >
+    import Greet from "./components/greet.vue"; 
+    import Article from "./components/Article.vue"
+    export default {
+        name: "App", 
+        data(){
+            return {
+                name:"Kay Z", 
+                heroName: "super dev", 
+                someTitle:"Avengers"
+            }
+        }, 
+        components: {
+            Greet, 
+            Article
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/src/components/Greet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     <h2>Hello {{ name}} a.K.a {{ heroName }}  </h2>
+    </div>
+</template>
+
+<script >
+    export default {
+        name: "Greet", 
+        props:["name", "heroName"],
+        data(){
+            return {
+                test: "kayTesting"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
